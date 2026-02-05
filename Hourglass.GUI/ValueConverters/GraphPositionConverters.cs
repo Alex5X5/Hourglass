7b08134e30a096fb90d9c0c30a3b9047cf69577a
Alex5X5 <alex.25.lehnert@gmail.com>
2026-02-05 16:02:20 +0100

    fixed size calculation of task controlls

--- Hourglass.GUI/ValueConverters/GraphPositionConverters.cs
+++ Hourglass.GUI/ValueConverters/GraphPositionConverters.cs
@@ -4,22 +4,9 @@ using Avalonia;
 using Avalonia.Data.Converters;
 using Hourglass.GUI.ViewModels.Components;
 using Hourglass.GUI.ViewModels.Components.GraphPanels;
+using Hourglass.GUI.Views.Components.GraphPanels;
 using System.Collections.Generic;
 using System.Globalization;
-
-public class ProductDisplayConverter : IMultiValueConverter {
-    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture) {
-        // First value is the entire Product object
-        if (values[0] is Product product) {
-            var discount = values.Count > 1 && values[1] is double d ? d : 0;
-            var finalPrice = product.Price * (1 - (decimal)discount / 100);
-
-            return $"{product.Name} - {finalPrice:F2} {product.Currency} ({product.Description})";
-        }
-
-        return "Invalid product";
-    }
-}
 
 public class CanvasLeftConverter : IMultiValueConverter {
     public object? Convert(IList<object?> lst, Type targetType, object? parameter, CultureInfo culture) {
@@ -40,6 +27,7 @@ public class CanvasTopConverter : IMultiValueConverter {
         double paddingY = parentBounds.Height * GraphPanelViewModelBase.PADDING_Y_WEIGHT / (GraphPanelViewModelBase.GRAPH_AREA_Y_WEIGHT + 2 * GraphPanelViewModelBase.PADDING_Y_WEIGHT);
         double graphAreaHeight = parentBounds.Height - 2 * paddingY;
         double yAxissegmentSize = graphAreaHeight / (panelModel?.Y_AXIS_SEGMENT_COUNT ?? 1 * 1.5) * panelModel?.TASK_GRAPH_COLUMN_COUNT ?? 1;
+        return 500.0;
         return yAxissegmentSize * (taskModel?.index ?? 0 % (panelModel?.MAX_TASKS ?? 10 / panelModel?.TASK_GRAPH_COLUMN_COUNT ?? 1)) * 1.5 + paddingY;
     }
 }
@@ -47,11 +35,16 @@ public class CanvasTopConverter : IMultiValueConverter {
 public class WidthConverter : IMultiValueConverter {
     public object? Convert(IList<object?> lst, Type targetType, object? parameter, CultureInfo culture) {
         Rect parentBounds = (lst[0] as Rect?) ?? new Rect(0, 0, 10, 10);
-        GraphPanelViewModelBase? panelModel = lst[1] as GraphPanelViewModelBase;
-        TaskGraphViewModel? taskModel = lst[2] as TaskGraphViewModel;
-        double proportion = parentBounds.Width / panelModel?.TIME_INTERVALL_START_SECONDS ?? 1;
+        GraphPanelViewModelBase? panel = lst[1] as GraphPanelViewModelBase;
+        TaskGraphViewModel? task = lst[2] as TaskGraphViewModel;
+        long start = task?.task.start ?? 0;
+        long finish = task?.task.finish ?? 1;
+        bool running = task?.task.running ?? false;
         double paddingX = parentBounds.Width * GraphPanelViewModelBase.PADDING_X_WEIGHT / (GraphPanelViewModelBase.GRAPH_AREA_X_WEIGHT + 2 * GraphPanelViewModelBase.PADDING_X_WEIGHT);
-        return (taskModel?.task.start ?? 0 - panelModel?.TIME_INTERVALL_START_SECONDS ?? 0) * proportion + paddingX;
+        double proportion = (parentBounds.Width - 2 * paddingX) / panel?.TIME_INTERVALL_DURATION ?? 1;
+        long duration = running ? DateTimeService.ToSeconds(DateTime.Now) - start : finish - start;
+        double graphLength = duration * proportion;
+        return Math.Max(graphLength, panel?.GRAPH_MINIMAL_WIDTH ?? 10)*2;
     }
 }
 
